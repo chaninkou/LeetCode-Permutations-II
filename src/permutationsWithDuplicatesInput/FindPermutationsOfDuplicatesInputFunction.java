3c53afd6e887d85eee3cf21a359a4ff025288341
chaninkou <michaelkoulove@yahoo.com>
2019-11-26 23:54:29 -0800

    Great review on backtracking, with tempList, boolean array for duplicate

--- src/permutationsWithDuplicatesInput/FindPermutationsOfDuplicatesInputFunction.java
+++ src/permutationsWithDuplicatesInput/FindPermutationsOfDuplicatesInputFunction.java
@@ -20,6 +20,8 @@ public class FindPermutationsOfDuplicatesInputFunction {
     private void backtrack(List<List<Integer>> finalList, List<Integer> tempList, int[] nums, boolean[] used){
         // Permutation solution will have the same length as input
         if(tempList.size() == nums.length){
+        	
+        	// Make sure to add a new arraylist or else its not going to add anything
             finalList.add(new ArrayList<>(tempList));
         } else {
             for(int i = 0; i < nums.length; i++){
